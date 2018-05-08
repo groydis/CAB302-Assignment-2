be67d4284f1838c6b078e240cfd7f7a83e5edd72
KALVS <a.holm57@hotmail.com>
2018-05-08 17:48:46 +1000

    Made Documents Tab to agreed design
    Folder Icons, load buttons and better designs for document tab.
    also changed TextArea to editable to type file directory in area

--- Project/src/GUI/GUI.java
+++ Project/src/GUI/GUI.java
@@ -29,8 +29,10 @@ public class GUI extends JFrame implements Observer, ActionListener
 
 	JFrame mainFrame = new JFrame();
 	JTabbedPane pane = new JTabbedPane();
+	
 	//Store Variables
 	JPanel storeTab = new JPanel(); 
+	
 	//Inventory Variables
     JPanel inventoryTab  = new JPanel();
     String[] inventoryColumnNames
@@ -41,24 +43,40 @@ public class GUI extends JFrame implements Observer, ActionListener
     		{"Name", "Cost", "Price", "Reorder Point", "Reorder Amount", "Temperature"},
     		};
     JTable inventoryTable = new JTable(data, inventoryColumnNames);
+    
     //Document Variables
     JPanel documentTab = new JPanel();
     
+    ImageIcon folderIcon = new ImageIcon("images/OpenFolder.png", "Open the File Chooser");
+    
+    
+    //Items for the properties section. With A label, text area, a filechooser
+    //icon and buttons
+    JLabel propLabel = new JLabel("Item Properties");
     JTextArea propertiesTextArea = new JTextArea(1, 20);
     JFileChooser propertiesChooser = new JFileChooser();
-    JButton propertiesButton = new JButton("Load Item Properties");
-    
-    JTextArea exportTextArea = new JTextArea(1, 20);
-    JFileChooser exportChooser = new JFileChooser();
-    JButton exportButton = new JButton("Export Manifest");
-    
+    JButton propertiesChooseButton = new JButton(folderIcon);
+    JButton propertiesButton = new JButton("Load");
+
+    JLabel maniLabel = new JLabel("Manifests");
     JTextArea manifestTextArea = new JTextArea(1, 20);
     JFileChooser manifestChooser = new JFileChooser();
-    JButton manifestButton = new JButton("Load Manifest");
+    JButton manifestChooseButton = new JButton(folderIcon);
+    JButton manifestButton = new JButton("Load");
     
+    JLabel exportLabel = new JLabel("Export Manifest");
+    JTextArea exportTextArea = new JTextArea(1, 20);
+    JFileChooser exportChooser = new JFileChooser();
+    JButton exportChooseButton = new JButton(folderIcon);
+    JButton exportButton = new JButton("Export");
+    
+    JLabel salesLabel = new JLabel("Sales Log");
     JTextArea salesTextArea = new JTextArea(1, 20);
     JFileChooser salesChooser = new JFileChooser();
-    JButton salesButton = new JButton("Load Sales Logs");
+    JButton salesChooseButton = new JButton(folderIcon);
+    JButton salesButton = new JButton("Load");
+    
+
     
     
 	public GUI() {
@@ -88,10 +106,10 @@ public class GUI extends JFrame implements Observer, ActionListener
 
         pane.add("Documents", documentTab);
         
-        propertiesTextArea.setEditable(false);
-        exportTextArea.setEditable(false);
-        manifestTextArea.setEditable(false);
-        salesTextArea.setEditable(false);
+        propertiesTextArea.setEditable(true);
+        exportTextArea.setEditable(true);
+        manifestTextArea.setEditable(true);
+        salesTextArea.setEditable(true);
         
         //This can change to files only or files and directories but i dunno what was a good choice
         propertiesChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
@@ -104,43 +122,69 @@ public class GUI extends JFrame implements Observer, ActionListener
 	    manifestChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         salesChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 	    //Make properties Text area and button to open file choose
-        propertiesButton.addActionListener(this);
-        manifestButton.addActionListener(this);
-        exportButton.addActionListener(this);
-        salesButton.addActionListener(this);
+        propertiesChooseButton.addActionListener(this);
+        manifestChooseButton.addActionListener(this);
+        exportChooseButton.addActionListener(this);
+        salesChooseButton.addActionListener(this);
+        
         
         documentTab.setLayout(new GridBagLayout());
         GridBagConstraints docuTabLayout = new GridBagConstraints();
         docuTabLayout.fill = GridBagConstraints.HORIZONTAL;
         docuTabLayout.anchor = GridBagConstraints.WEST;
-        
-        docuTabLayout.gridy = 1;
-        
+
+        docuTabLayout.gridy = 0;
         docuTabLayout.gridx = 0;
+        documentTab.add(propLabel, docuTabLayout);
+        docuTabLayout.gridy = 1;
         documentTab.add(propertiesTextArea, docuTabLayout);
+        docuTabLayout.gridx = 1;
+        documentTab.add(propertiesChooseButton, docuTabLayout);
         docuTabLayout.gridx = 2;
         documentTab.add(propertiesButton, docuTabLayout);
         
         docuTabLayout.gridy = 2;
-        
         docuTabLayout.gridx = 0;
-        documentTab.add(exportTextArea, docuTabLayout);
-        docuTabLayout.gridx = 2;
-        documentTab.add(exportButton, docuTabLayout);
-
+        documentTab.add(maniLabel, docuTabLayout);
         docuTabLayout.gridy = 3;
-        
-        docuTabLayout.gridx = 0;
         documentTab.add(manifestTextArea, docuTabLayout);
+        docuTabLayout.gridx = 1;
+        documentTab.add(manifestChooseButton, docuTabLayout);
         docuTabLayout.gridx = 2;
         documentTab.add(manifestButton, docuTabLayout);
-        
+
         docuTabLayout.gridy = 4;
-        
         docuTabLayout.gridx = 0;
+        documentTab.add(exportLabel, docuTabLayout);
+        docuTabLayout.gridy = 5;
+        documentTab.add(exportTextArea, docuTabLayout);
+        docuTabLayout.gridx = 1;
+        documentTab.add(exportChooseButton, docuTabLayout);
+        docuTabLayout.gridx = 2;
+        documentTab.add(exportButton, docuTabLayout);
+        
+        docuTabLayout.gridy = 6;
+        docuTabLayout.gridx = 0;
+        documentTab.add(exportLabel, docuTabLayout);
+        docuTabLayout.gridy = 7;
+        documentTab.add(exportTextArea, docuTabLayout);
+        docuTabLayout.gridx = 1;
+        documentTab.add(exportChooseButton, docuTabLayout);
+        docuTabLayout.gridx = 2;
+        documentTab.add(exportButton, docuTabLayout);
+        
+        docuTabLayout.gridy = 8;
+        docuTabLayout.gridx = 0;
+        documentTab.add(salesLabel, docuTabLayout);
+        docuTabLayout.gridy = 9;
         documentTab.add(salesTextArea, docuTabLayout);
+        docuTabLayout.gridx = 1;
+        documentTab.add(salesChooseButton, docuTabLayout);
         docuTabLayout.gridx = 2;
         documentTab.add(salesButton, docuTabLayout);
+        
+        
+        
         
         getContentPane().add(pane);
         
@@ -164,7 +208,7 @@ public class GUI extends JFrame implements Observer, ActionListener
     }
 	
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == propertiesButton) {
+		if(e.getSource() == propertiesChooseButton) {
 			int returnVal = propertiesChooser.showOpenDialog(GUI.this);
 
 		      if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -175,13 +219,13 @@ public class GUI extends JFrame implements Observer, ActionListener
 		      }
 		}
 		//this SAVES a file
-		else if (e.getSource() == exportButton) {
+		else if (e.getSource() == exportChooseButton) {
 		      int returnVal = exportChooser.showSaveDialog(GUI.this);
 		      if (returnVal == JFileChooser.APPROVE_OPTION) {
 		        File file = exportChooser.getSelectedFile();
 		      }
 		}
-		else if (e.getSource() == manifestButton) {
+		else if (e.getSource() == manifestChooseButton) {
 			int returnVal = manifestChooser.showOpenDialog(GUI.this);
 
 		      if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -191,7 +235,7 @@ public class GUI extends JFrame implements Observer, ActionListener
 		        manifestTextArea.append(filename);
 		      }
 		}
-		else if (e.getSource() == salesButton) {
+		else if (e.getSource() == salesChooseButton) {
 			int returnVal = salesChooser.showOpenDialog(GUI.this);
 
 		      if (returnVal == JFileChooser.APPROVE_OPTION) {
